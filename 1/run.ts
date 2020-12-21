4eee8fd4c8009fbcfcd9d80e9f0ea15496cfba7c
zainrax <patrickb321@gmail.com>
2020-12-21 16:00:11 +1300

    fix: remone ts file

--- 1/run.ts
+++
@@ -1,9 +0,0 @@
-const file = await Deno.readTextFile("./input.txt");
-let input = file.split("\n");
-const target = 2020;
-const table = {};
-
-input.forEach((item) => {
-  const num = parseInt(item, 10);
-  table[num] = target - num;
-});
